d5d3aecc82877abdb597461e61b912f1690f7f7d
Blair <abrgroup@gmail.com>
2019-12-05 21:54:08 -0500

    added 3 events

--- js/index.js
+++ js/index.js
@@ -1,5 +1,21 @@
 // Your code goes here
+
+// add mouseover event
 const h1Tag = document.querySelector(".logo-heading");
 h1Tag.addEventListener("mouseover", () => {
   h1Tag.style.color = "red";
+});
+
+// add a click event
+const sectionTitles = document.querySelectorAll("h2");
+sectionTitles.forEach(title => {
+  title.addEventListener("click", () => {
+    title.style.color = "green";
+  });
+});
+
+// resize events
+const busImg = document.querySelector(".intro img");
+window.addEventListener("resize", () => {
+  busImg.src = "img/newbus.jpg";
 });
